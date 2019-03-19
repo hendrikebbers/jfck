19a662f2774a70209b4dc4a4c485a17068aa17dc
Hendrik Ebbers <hendrik.ebbers@web.de>
2019-03-19 21:05:17 +0100

    Controller

---
+++ server-spring/src/main/java/eu/javaland/ck/DataConstants.java
@@ -0,0 +1,17 @@
+package eu.javaland.ck;
+
+public interface DataConstants {
+
+    int GUNNAR_ID = 1;
+
+    int ANDREAS_ID = 2;
+
+    int SIMONS_ID = 3;
+
+    int ED_ID = 4;
+
+    int MICHAEL_ID = 5;
+
+    int GEAL_ID = 6;
+
+}
--- server-spring/src/main/java/eu/javaland/ck/rico/TeamsController.java
+++ server-spring/src/main/java/eu/javaland/ck/rico/TeamsController.java
@@ -1,16 +1,31 @@
 package eu.javaland.ck.rico;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import dev.rico.server.remoting.Param;
 import dev.rico.server.remoting.RemotingAction;
 import dev.rico.server.remoting.RemotingController;
 import dev.rico.server.remoting.RemotingModel;
 import dev.rico.server.remoting.event.RemotingEventBus;
 import eu.javaland.ck.CommunityEventTopics;
+import eu.javaland.ck.DataConstants;
+import eu.javaland.ck.Database;
+import eu.javaland.ck.TeamConstants;
 import eu.javaland.ck.TeamsModel;
+import eu.javaland.ck.models.Vote;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.PostConstruct;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static eu.javaland.ck.DataConstants.ANDREAS_ID;
+import static eu.javaland.ck.DataConstants.ED_ID;
+import static eu.javaland.ck.DataConstants.GEAL_ID;
 import static eu.javaland.ck.TeamConstants.SET_TEAM_ACTION;
 import static eu.javaland.ck.TeamConstants.TEAM;
 import static eu.javaland.ck.TeamConstants.TEAM_CONTROLLER;
@@ -26,18 +41,61 @@ public class TeamsController {
 
     private String currentTeam;
 
+    private final Gson gson = new Gson();
+
     @PostConstruct
     public void init() {
         eventBus.subscribe(CommunityEventTopics.UPDATE, e -> update());
     }
 
     private void update() {
-        model.setTitle("Buildtools");
-        model.setValueOneTitle("Maven");
-        model.setValueTwoTitle("Gradle");
+        if(Objects.equals(currentTeam, TeamConstants.BUILD_TOOLS)) {
+            model.setTitle("Buildtools");
+            model.setValueOneTitle("Gradle");
+            model.setValueTwoTitle("Maven");
+        }
+        if(Objects.equals(currentTeam, TeamConstants.ENTERPRISE)) {
+            model.setTitle("Enterprise Frameworks");
+            model.setValueOneTitle("Jakarta EE");
+            model.setValueTwoTitle("Spring");
+        }
+        if(Objects.equals(currentTeam, TeamConstants.IDES)) {
+            model.setTitle("Buildtools");
+            model.setValueOneTitle("Eclipse");
+            model.setValueTwoTitle("Netbeans");
+        }
 
-        model.setValueOne(Math.random());
-        model.setValueTwo(Math.random());
+        AtomicInteger countA = new AtomicInteger();
+        AtomicInteger countB = new AtomicInteger();
+
+        Database.votes.forEach((k, v) -> {
+            final Vote vote = gson.fromJson(v, Vote.class);
+            if (isCorrect(vote)) {
+                if (vote.getSpeakerId() == ANDREAS_ID || vote.getSpeakerId() == ED_ID || vote.getSpeakerId() == GEAL_ID) {
+                    countA.incrementAndGet();
+                } else {
+                    countB.incrementAndGet();
+                }
+            }
+        });
+        final int sum = countA.intValue() + countB.intValue();
+        final double aPercantage = countA.doubleValue() / sum;
+        final double bPercantage = countB.doubleValue() / sum;
+
+        model.setValueOne(aPercantage);
+        model.setValueTwo(bPercantage);
+    }
+
+    private boolean isCorrect(final Vote vote) {
+        try {
+            JsonParser parser = new JsonParser();
+            final JsonElement element = parser.parse(new FileReader(TeamsController.class.getResource("questions.json").getFile()));
+            final JsonElement questionElement = element.getAsJsonArray().get(vote.getQuestionId());
+            final int answerIndex = questionElement.getAsJsonObject().get("solution").getAsInt();
+            return vote.getQuestionAnswer() == answerIndex;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @RemotingAction(SET_TEAM_ACTION)
