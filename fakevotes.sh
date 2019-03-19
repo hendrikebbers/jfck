6886c9e67c978d36201a0651ab5c46869a1501c8
Christoph Engelbert <me@noctarius.com>
2019-03-19 21:28:40 +0100

    fake vooooooooooooooting

--- fakevotes.sh
+++ fakevotes.sh
@@ -1,1 +1,12 @@
 #!/usr/bin/env bash
+
+userId=0
+while true; do
+    questionAnswer=$(( (RANDOM % 4) + 1))
+    speakerId=$(( (RANDOM % 2) + 1))
+    body="{'id':'fooo', 'userId':'$userId','speakerId':$speakerId, 'speakerName': '', 'questionId': 1, 'questionAnswer': $questionAnswer}"
+    echo $body
+    curl -H "Content-Type: application/json" -d "$body" -XPUT http://localhost:8080/votes
+    sleep 1
+    userId=$((userId+1))
+done;
