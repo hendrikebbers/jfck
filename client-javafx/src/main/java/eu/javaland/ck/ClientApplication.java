b4d1249cc83355056e7d50ecbd017e6889dd2fef
Hendrik Ebbers <hendrik.ebbers@web.de>
2019-03-19 21:21:32 +0100

    all tems

--- client-javafx/src/main/java/eu/javaland/ck/ClientApplication.java
+++ client-javafx/src/main/java/eu/javaland/ck/ClientApplication.java
@@ -4,6 +4,7 @@ import dev.rico.client.remoting.AbstractRemotingApplication;
 import dev.rico.client.remoting.ClientContext;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import java.net.MalformedURLException;
@@ -18,17 +19,26 @@ public class ClientApplication extends AbstractRemotingApplication {
     }
 
     protected void start(Stage primaryStage, ClientContext clientContext) throws Exception {
-        final Button buildToolsButton = new Button("BuildTools");
-        buildToolsButton.setOnAction(e -> {
-            final TeamViewController teamViewController = new TeamViewController(clientContext, BUILD_TOOLS);
+        final Button buildToolsButton = createTeamButton(clientContext, BUILD_TOOLS, "Build tools");
+        final Button enterpriseButton = createTeamButton(clientContext, BUILD_TOOLS, "Build tools");
+        final Button ideButton = createTeamButton(clientContext, BUILD_TOOLS, "Build tools");
+        final HBox box = new HBox(buildToolsButton, enterpriseButton, ideButton);
+
+        primaryStage.setScene(new Scene(box));
+        primaryStage.show();
+    }
+
+    private Button createTeamButton(final ClientContext clientContext, final String teamId, final String buttonText) {
+        final Button button = new Button(buttonText);
+        button.setOnAction(e -> {
+            final TeamViewController teamViewController = new TeamViewController(clientContext, teamId);
             final Stage stage = new Stage();
             stage.setScene(new Scene(teamViewController.getParent()));
             stage.show();
         });
-
-        primaryStage.setScene(new Scene(buildToolsButton));
-        primaryStage.show();
+        return button;
     }
+
 
     public static void main(String[] args) {
         launch();
